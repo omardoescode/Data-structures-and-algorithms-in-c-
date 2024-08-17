cae1ad60042fec8a0d6287b0b25b62468216bcd2
omardoescode <omardoescode@gmail.com>
2024-08-17 19:35:43 +0300

    Quick sort standard routine

---
+++ algorithms/sorting/quicksort.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+template <typename Comparable>
+const Comparable &median3(std::vector<Comparable> &a, int left, int right) {
+  int center = (left + right) / 2;
+  if (a[center] < a[left])
+    std::swap(a[left], a[center]);
+  if (a[right] < a[left])
+    std::swap(a[left], a[right]);
+  if (a[right] < a[center])
+    std::swap(a[center], a[right]);
+
+  std::swap(a[center], a[right - 1]);
+  return a[right - 1];
+}
+
+template <typename Comparable>
+void quicksort(std::vector<Comparable> &a, int left, int right) {
+  // base case
+  if (left + 10 > right)
+    return insertion_sort(a);
+
+  const Comparable &pivot = median(a, left, right);
+
+  int i = left, j = right - 1;
+  for (;;) {
+    while (a[++i] < pivot)
+      ;
+    while (a[++j] < pivot)
+      ;
+
+    if (i < j)
+      std::swap(a[i], a[j]);
+    else
+      break;
+  }
+
+  std::swap(a[i], a[right - 1]);
+
+  quicksort(a, left, i - 1);
+  quicksort(a, i + 1, right);
+}
+template <typename Comparable> void quicksort(std::vector<Comparable> &a) {
+  quicksort(a, 0, a.size() - 1);
+}
